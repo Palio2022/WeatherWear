a18f9c5f86d90f76e0fffdd4fda2cdf65272f9cf
Pascal Littek <pascal.littek@telekom.de>
2024-07-14 13:08:37 +0200

    complete Project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from "vue";
+import Kachel from "@/components/kachel.vue";
+
+export default defineComponent({
+  components: {Kachel}
+})
+</script>
+
+<template>
+  <main>
+    <kachel daytime="Vormittags"></kachel>
+    <kachel daytime="Mittag"></kachel>
+    <kachel></kachel>
+    <kachel></kachel>
+  </main>
+</template>
