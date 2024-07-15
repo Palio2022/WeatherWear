900a42ccebafa081b56f0ace99fa79d3f394428c
Jonas Gutbrod <jonas.gutbrod@igcv.fraunhofer.de>
2024-07-15 15:05:11 +0200

    Add Weathercode

--- src/components/kachel.vue
+++ src/components/kachel.vue
@@ -1,14 +1,30 @@
 <template>
-  
-     <div class="border hero ">
+
+  <div class="border hero ">
     <div class="hero-body has-text-black kachel ">
       <div class="has-text-centered ">
-        <div class="title is-4 has-text-black"> {{this.daytime}}</div>
-        <div class="title is-5 has-text-weight-bold has-text-black "> <img src="https://www.svgrepo.com/show/471978/thermometer-03.svg" alt="IMG NOT FOUND" style="width: 45px;">{{ this.temperature }} °C</div>
-        <div class="title is-5 has-text-weight-bold has-text-black"> <img src="https://www.svgrepo.com/show/466946/wind-alt.svg" alt="IMG NOT FOUND" style="width: 45px;"> {{ this.wind }} km/h</div>
-        <div class="title is-5 has-text-weight-bold has-text-black"> <img src="https://www.svgrepo.com/show/451222/rain.svg" alt="IMG NOT FOUND" style="width: 45px;">{{ this.rain }} mm</div>
-      
-<!--        <div><img src="" alt="Kleidungsstück"></div>-->
+        <div class="title is-4 has-text-black"> {{ this.daytime }}</div>
+        <div class="container" style="font-size: large;">
+          <div style="display: flex; align-items: center; margin-left: 10px;">
+            <img src="https://www.svgrepo.com/show/471978/thermometer-03.svg" alt="IMG NOT FOUND" style="width: 25px;">
+            <span style="margin-left: 3px;">{{ this.temperature }}°C</span>
+          </div>
+          <div style="display: flex; align-items: center; margin-left: 10px;">
+             <img src="https://www.svgrepo.com/show/466946/wind-alt.svg" alt="IMG NOT FOUND" style="width: 25px;"> 
+             
+            <span >{{this.wind}}km/h</span> 
+          </div>
+         <div style="display: flex; align-items: center; margin-left: 10px;">
+           <img src="https://www.svgrepo.com/show/451222/rain.svg" alt="IMG NOT FOUND" style="width:25px;">
+           <span style="margin-left: 3px;">
+            {{this.rain}}mm
+           </span>
+           <div>{{ this.weathertyp }}</div>
+           
+         </div>
+         
+        </div>
+        <!--<div><img src="" alt="Kleidungsstück"></div>-->
         <div>{{ kleidungsstueck }}</div>
       </div>
       <div>
@@ -17,87 +33,99 @@
       </div>
     </div>
   </div>
-  
- 
+
+
 </template>
 
 <script>
-import {stringifyQuery} from "vue-router";
+import { stringifyQuery } from "vue-router";
 
 export default {
   props: {
     daytime: String,
     temperature: Number,
-    weathertyp: String,  //rain, sun, snow
+    weathertyp: Number,  
     uvIndex: Number,
     wind: Number,
     rain: Number
   },
   computed: {
     uvMaßnahme() {
-      if(this.uvIndex <= 2){
+      if (this.uvIndex <= 2) {
         return "Kein UV Schutz notwendig"
       }
-      else if(this.uvIndex >= 3 && this.uvIndex <= 5){
+      else if (this.uvIndex >= 3 && this.uvIndex <= 5) {
         return "Entweder Hut, T-Shirt, Sonnenbrille, Sonnencreme tragen oder im Schatten aufhalten"
       }
-      else if(this.uvIndex >= 6 && this.uvIndex <= 7){
+      else if (this.uvIndex >= 6 && this.uvIndex <= 7) {
         return "Hut, T-Shirt, Sonnenbrille, Sonnencreme und im Schatten aufhalten"
       }
-      else if(this.uvIndex >= 8 && this.uvIndex <= 10){
+      else if (this.uvIndex >= 8 && this.uvIndex <= 10) {
         return "Hut, T-Shirt, Sonnenbrille, Sonnencreme, im Schatten aufhalten, zwischen 11 und 16 Uhr im Haus sein"
       }
     },
 
     kleidungsstueck() {
-      if (this.weathertyp.includes("rain") || this.weathertyp.includes("snow")){
+      if (this.weathertyp >= 51 && this.weathertyp <= 99) {
         return "Jacke"
       }
-      else if (this.weathertyp.includes("sun")){
-        if(this.temperature < 10){
+      else {
+        if (this.temperature < 10) {
           return "Jacke, Pullover, Lange Hose"
         }
-        else if (this.temperature >= 10 && this.temperature <= 17 ){
+        else if (this.temperature >= 10 && this.temperature <= 17) {
           return "Pullover, Lange Hose";
         }
-        else if (this.temperature >= 16 && this.temperature <= 20 ){
+        else if (this.temperature >= 16 && this.temperature <= 20) {
           return "T-Shirt, Lange Hose";
         }
-        else if (this.temperature >= 21 && this.temperature <= 24 ){
+        else if (this.temperature >= 21 && this.temperature <= 24) {
           return "Pullover, Kurze Hose";
         }
-        else if (this.temperature >= 25 ){
+        else if (this.temperature >= 25) {
           return "T-Shirt, Kurze Hose";
         }
       }
+        
+      
     }
   },
 }
 </script>
 
 <style>
+.container {
+  display: flex;
+
+}
+
+
 .date {
   font-size: xx-large;
   color: #001d3d;
   font-weight: bold;
 }
-.outerBorder{
+
+.outerBorder {
   margin: 15px;
   padding: 10px;
   border-style: solid;
   border-color: #D4A373;
   border-radius: 20px;
 }
+
 .kachel {
   background-color: #CCD5AE;
   border-radius: 20px;
 }
+
 body {
   background-color: #FAEDCD;
 }
-.border{
+
+.border {
   margin: 10px;
-  padding: 10px;
+  padding: 5px;
   border-style: solid;
   border-color: white;
   border-radius: 20px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,17 @@ export default defineComponent({
   components: { Kachel },
   data() {
     return {
+      cityData: {
+        
+      },
       weatherData: {
         morning: {
           temperature: null,
           rain: null,
           sunshine: null,
           uv: null,
-          wind: null
+          wind: null,
+          weatherCode: 5
 
         },
         midday: {
@@ -20,28 +24,32 @@ export default defineComponent({
           rain: null,
           sunshine: null,
           uv: null,
-          wind: null
+          wind: null,
+          weatherCode: 7
         },
         noon: {
           temperature: null,
           rain: null,
           sunshine: null,
           uv: null,
-          wind: null
+          wind: null,
+          weatherCode: 50
         },
         evening: {
           temperature: null,
           rain: null,
           sunshine: null,
           uv: null,
-          wind: null
+          wind: null,
+          weatherCode: 10
         },
         night: {
           temperature: null,
           rain: null,
           sunshine: null,
           uv: null,
-          wind: null
+          wind: null,
+          weatherCode: 1
         }
       },
       currentDate: '',
@@ -82,12 +90,14 @@ export default defineComponent({
       fetch(url)
         .then(response => response.json())
         .then(data => {
-          this.location = data.results[0].name || 'Unbekannt';
+          console.log("Fetched city data:", data);
+          this.displayCityName(data);
         })
         .catch(error => console.error('Error fetching city name:', error));
     },
     fetchData(latitude, longitude) {
-      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,wind_speed_10m,uv_index&timezone=Europe%2FBerlin&forecast_days=1&forecast_hours=24`;
+     
+      const url = "https://api.open-meteo.com/v1/forecast?latitude="+ latitude +"&longitude=" + longitude + "&hourly=temperature_2m,rain,weather_code,wind_speed_10m,uv_index&timezone=Europe%2FLondon&forecast_hours=24";
       fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -103,48 +113,54 @@ export default defineComponent({
       const sunshine = data.hourly.sunshine_duration || Array(hours.length).fill(0); // Falls keine sunshine_duration vorhanden ist
       const uvValues = data.hourly.uv_index;
       const wind = data.hourly.wind_speed_10m;
+      const weatherCode = data.hourly.weather_code;
 
       const morningIndex = this.findHourIndex(hours, 6);
       const middayIndex = this.findHourIndex(hours, 12);
       const noonIndex = this.findHourIndex(hours, 15);
       const eveningIndex = this.findHourIndex(hours, 18);
       const nightIndex = this.findHourIndex(hours, 0);
-
+      
       this.weatherData = {
         morning: {
           temperature: temperatures[morningIndex],
           rain: rain[morningIndex],
           sunshine: sunshine[morningIndex],
           uv: uvValues[morningIndex],
-          wind: wind[morningIndex]
+          wind: wind[morningIndex],
+          weatherCode: weatherCode[morningIndex]
         },
         midday: {
           temperature: temperatures[middayIndex],
           rain: rain[middayIndex],
           sunshine: sunshine[middayIndex],
           uv: uvValues[middayIndex],
-          wind: wind[middayIndex]
+          wind: wind[middayIndex],
+          weatherCode: weatherCode[middayIndex]
         },
         noon: {
           temperature: temperatures[noonIndex],
           rain: rain[noonIndex],
           sunshine: sunshine[noonIndex],
           uv: uvValues[noonIndex],
-          wind: wind[nightIndex]
+          wind: wind[nightIndex],
+          weatherCode: weatherCode[noonIndex]
         },
         evening: {
           temperature: temperatures[eveningIndex],
           rain: rain[eveningIndex],
           sunshine: sunshine[eveningIndex],
           uv: uvValues[eveningIndex],
-          wind: wind[eveningIndex]
+          wind: wind[eveningIndex],
+          weatherCode: weatherCode[eveningIndex]
         },
         night: {
           temperature: temperatures[nightIndex],
           rain: rain[nightIndex],
           sunshine: sunshine[nightIndex],
           uv: uvValues[nightIndex],
-          wind: wind[nightIndex]
+          wind: wind[nightIndex],
+          weatherCode: weatherCode[nightIndex]
         }
       };
 
@@ -157,29 +173,36 @@ export default defineComponent({
       const date = new Date();
       this.currentDate = date.toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
-    }
+    },
+    displayCityName(data) {
+
+    this.cityData = data.address.city + ", " + data.address.country;
+    console.log("City Name:", this.location);
+  }
   },
+  
+  
 
 });
 </script>
 
 <template>
-  <span class="date"> {{ currentDate }} {{ location }} </span>
-  <div><img src="https://www.svgrepo.com/show/513450/location-pin.svg" alt="IMG NOT FOUND" style="width: 45px;"><span>{{
-    location }}</span></div>
+  <span class="date"> {{ currentDate }}  </span>
+  <div class="date"><img src="https://www.svgrepo.com/show/513450/location-pin.svg" alt="IMG NOT FOUND" style="width: 45px;"><span>{{
+    cityData }}</span></div>
   <main class="columns is-fullwidth outerBorder">
-    <kachel class="column" daytime="Vormittags" :uv-index="weatherData.morning.uv" weathertyp="rain"
+    <kachel class="column" daytime="Vormittags" :uv-index="weatherData.morning.uv" :weathertyp="weatherData.morning.weatherCode"
       :temperature="weatherData.morning.temperature" :wind="weatherData.morning.wind" :rain="weatherData.morning.rain">
     </kachel>
-    <kachel class="column" daytime="Mittags" :uv-index="weatherData.midday.uv" weathertyp="sun"
+    <kachel class="column" daytime="Mittags" :uv-index="weatherData.midday.uv" :weathertyp="weatherData.midday.weatherCode"
       :temperature="weatherData.midday.temperature" :wind="weatherData.midday.wind" :rain="weatherData.midday.rain">
     </kachel>
-    <kachel class="column" daytime="Nachmittag" :uv-index="weatherData.noon.uv" weathertyp="sun"
+    <kachel class="column" daytime="Nachmittag" :uv-index="weatherData.noon.uv" :weathertyp="weatherData.noon.weatherCode"
       :temperature="weatherData.noon.temperature" :wind="weatherData.noon.wind" :rain="weatherData.noon.rain"></kachel>
-    <kachel class="column" daytime="Abend" :uv-index="weatherData.evening.uv" weathertyp="sun"
+    <kachel class="column" daytime="Abend" :uv-index="weatherData.evening.uv" :weathertyp="weatherData.evening.weatherCode"
       :temperature="weatherData.evening.temperature" :wind="weatherData.evening.wind" :rain="weatherData.evening.rain">
     </kachel>
-    <kachel class="column" daytime="Nacht" :uv-index="weatherData.night.uv" weathertyp="sun"
+    <kachel class="column" daytime="Nacht" :uv-index="weatherData.night.uv" :weathertyp="weatherData.night.weatherCode"
       :temperature="weatherData.night.temperature" :wind="weatherData.night.wind" :rain="weatherData.night.rain">
     </kachel>
   </main>
